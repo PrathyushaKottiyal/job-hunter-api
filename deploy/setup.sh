6115035caa0ad8262b423c6aac2bafff5ebcc142
PrathyushaKottiyal <prathyusha2502@gmail.com>
2019-11-19 01:23:49 +0530

    path change

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/PrathyushaKottiyal/job-hunter-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/job-hunter'
+PROJECT_BASE_PATH='/usr/local/apps/job-hunter-api'
 
 echo "Installing dependencies..."
 apt-get update
