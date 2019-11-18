1870d779f060aa89f69d0b41f5dfd00fde7d9450
PrathyushaKottiyal <prathyusha2502@gmail.com>
2019-11-19 00:27:24 +0530

    setup change

--- jobhunter_project/settings.py
+++ jobhunter_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a%bkdf(q)&fs-k9hbdnxy$j$k8h$ybx37a-5@2veb@n@h6kn+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL ='jobhunter_api.UserProfile'
+
+STATIC_ROOT ='static/'
