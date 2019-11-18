228f46cac767264c377b13d9728ebbfe105d1a4e
PrathyushaKottiyal <prathyusha2502@gmail.com>
2019-11-19 02:26:24 +0530

    change

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -37,7 +37,7 @@ supervisorctl restart jobhunter_api
 
 # Configure nginx
 cp $PROJECT_BASE_PATH/deploy/nginx_jobhunter_api.conf /etc/nginx/sites-available/jobhunter_api.conf
-rm /etc/nginx/sites-enabled/default
+rm -rf /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/jobhunter_api.conf /etc/nginx/sites-enabled/jobhunter_api.conf
 systemctl restart nginx.service
 
