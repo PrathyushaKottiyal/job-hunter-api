a19c69b39f66ec3e828bfa385ef04370be1b2d49
PrathyushaKottiyal <prathyusha2502@gmail.com>
2019-11-19 01:53:20 +0530

    change

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -12,15 +12,15 @@ apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
-# mkdir -p $PROJECT_BASE_PATH
-# git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+mkdir -p $PROJECT_BASE_PATH
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Create virtual environment
-# mkdir -p $PROJECT_BASE_PATH/env
+mkdir -p $PROJECT_BASE_PATH/env
 python3 -m venv $PROJECT_BASE_PATH/env
 
 # Install python packages
-sudo -H $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+sudo -H $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.text
 sudo -H $PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
 # Run migrations and collectstatic
